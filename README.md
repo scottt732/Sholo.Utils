e4272b66e575babb02f504e6ac3bcb5c3dd4a54d
Scott Holodak <scottt732@gmail.com>
2020-06-26 02:23:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+![Banner](Images/Banner.png)
+
+# Sholo.Utils
+
+Example showing how to setup an Azure Pipelines build status badge and build history bar chart:
+```md
+[![Azure Pipelines Overall Build Status](https://dev.azure.com/dotnet-boxed/Templates/_apis/build/status/Dotnet-Boxed.Templates?branchName=master)](https://dev.azure.com/dotnet-boxed/Templates/_build/latest?definitionId=2&branchName=master)
+[![Azure Pipelines Build History](https://buildstats.info/azurepipelines/chart/dotnet-boxed/Templates/2?branch=master&includeBuildsFromPullRequest=false)](https://dev.azure.com/dotnet-boxed/Templates/_build/latest?definitionId=2&branchName=master)
+```
+
+Misc utility functions
---
+++ Source/Sholo.Utils/EnumExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Sholo.Utils
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+            => value.GetAttributeValue<DescriptionAttribute, string>(d => d.Description);
+
+        public static TAttribute GetAttribute<TAttribute>(this Enum value)
+            where TAttribute : Attribute
+            => value.GetAttributeValue<TAttribute, TAttribute>(t => t);
+
+        public static TValue GetAttributeValue<TAttribute, TValue>(this Enum value, Func<TAttribute, TValue> attributeValueSelector)
+            where TAttribute : Attribute
+            => value.GetType()
+                .GetMember(value.ToString())
+                .SelectMany(x => x.GetCustomAttributes(typeof(TAttribute), false))
+                .Cast<TAttribute>()
+                .Select(attributeValueSelector)
+                .FirstOrDefault();
+    }
+}
