698b8301574b05e4fc4985122044007624ddd29b
Scott Holodak <scottt732@gmail.com>
2020-06-27 14:05:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+![Banner](Images/Banner.png)
+
+# Sholo.Utils
+
+[![Sholo.Utils NuGet Package](https://img.shields.io/nuget/v/Sholo.Utils.svg)](https://www.nuget.org/packages/Sholo.Utils/)
+[![Twitter URL](https://img.shields.io/twitter/url/http/shields.io.svg?style=social)](https://twitter.com/scottt732)
+[![Twitter Follow](https://img.shields.io/twitter/follow/scottt732.svg?style=social&label=Follow)](https://twitter.com/scottt732)
+
+This repository contains miscellaneous utility functions and extension methods.  The packages here are depended upon by other Sholo packages.
---
+++ Source/Sholo.Utils/TypeExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sholo.Utils
+{
+    public static class TypeExtensions
+    {
+        // https://stackoverflow.com/a/8142597/179223
+        public static bool IsTypeOf<T>(this Type type)
+        {
+            return typeof(T).IsAssignableFrom(type);
+        }
+
+        public static bool IsTypeOf(this Type type, Type otherType)
+        {
+            return otherType.IsAssignableFrom(type);
+        }
+
+        // https://stackoverflow.com/a/5461399/179223 -> https://stackoverflow.com/a/1075059/1968
+        public static bool IsAssignableToGenericType(this Type type, Type genericType)
+        {
+            var interfaceTypes = type.GetInterfaces();
+
+            foreach (var it in interfaceTypes)
+            {
+                if (it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
+                    return true;
+            }
+
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == genericType)
+                return true;
+
+            Type baseType = type.BaseType;
+            if (baseType == null) return false;
+
+            return IsAssignableToGenericType(baseType, genericType);
+        }
+    }
+}
